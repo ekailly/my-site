bd1dec7a2ff736120f888e7881a3504b97a050ae
Eric Kailly <eric.jkailly@gmail.com>
2023-05-19 00:27:39 -0700

    remove color mode button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,23 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Link,
-  Text,
-  useColorMode,
-} from "@chakra-ui/react";
+import { Flex, Heading, Link, Text } from "@chakra-ui/react";
 
 const LINKS = [
   { label: "about", path: "/about" },
   { label: "contact", path: "/contact" },
 ];
 
-export const Header = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-
-  return (
-    <Flex direction="row" justify="center" align="center" padding="1.5rem">
-      <Flex align="center" mr={5}>
-        <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
-          <Link href="/">eric kailly</Link>
-        </Heading>
-      </Flex>
-      <Flex alignItems="center" direction="row">
-        {LINKS.map(({ label, path }) => (
-          <Text mr={6} display="block" key={label}>
-            <Link href={path}>{label}</Link>
-          </Text>
-        ))}
-        <Button onClick={toggleColorMode}>
-          {colorMode === "light" ? "🌜" : "🌞"}
-        </Button>
-      </Flex>
+export const Header = () => (
+  <Flex direction="row" justify="center" align="center" padding="1.5rem">
+    <Flex align="center" mr={5}>
+      <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+        <Link href="/">eric kailly</Link>
+      </Heading>
     </Flex>
-  );
-};
+    <Flex alignItems="center" direction="row">
+      {LINKS.map(({ label, path }) => (
+        <Text mr={6} display="block" key={label}>
+          <Link href={path}>{label}</Link>
+        </Text>
+      ))}
+    </Flex>
+  </Flex>
+);
