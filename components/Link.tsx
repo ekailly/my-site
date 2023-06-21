0e9f5c694df19e86110c78781bccfdfe407a7bea
Eric Kailly <eric.jkailly@gmail.com>
2023-06-21 10:11:41 -0700

    open links in new tab

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,11 @@
 import { Link as ChakraLink } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
-export const Link: React.FC<PropsWithChildren<{ href: string }>> = (props) => {
-  const { children, href } = props;
-
-  return (
-    <ChakraLink href={href} color="#46acc2" fontWeight="500">
-      {children}
-    </ChakraLink>
-  );
-};
+export const Link: React.FC<PropsWithChildren<{ href: string }>> = ({
+  children,
+  href,
+}) => (
+  <ChakraLink href={href} color="#46acc2" fontWeight="500" isExternal>
+    {children}
+  </ChakraLink>
+);
